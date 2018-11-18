a37129bd124da65eaf9153dd4adb4cf194748ac1
Virendra <virendra180kumar@gmail.com>
2018-11-18 15:35:04 +0530

    folder structer and adding a simple get method on new chart component

---
+++ vue-chart-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <bitcoin-chart></bitcoin-chart>
+  </div>
+</template>
+
+<script>
+import Chart from './components/Chart.vue'
+
+export default {
+  components : {
+    'bitcoin-chart' : Chart
+  },
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-chart-app/src/components/Chart.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>{{data}}</div>
+</template>
+
+<script>
+import {bus} from '../main'
+
+export default { 
+	data : function(){
+		return {
+			data : []
+		}
+	},
+	created : function(){
+		this.$http.get('https://api.coinmarketcap.com/v2/listings/').then(function(data){
+			console.log(data);
+			this.data = data;
+		})
+	}
+}
+</script>
+<style scoped>
+	
+</style>
